89d51b7065abc2f4c8c480a80bc201ff9a2b8231
Bryce Telfer <brycetelfer@allmanagedit.com.au>
2021-01-04 21:09:08 +1000

    Update status_available_spec.rb

--- spec/whois/parsers/responses/whois.audns.net.au/au/status_available_spec.rb
+++ spec/whois/parsers/responses/whois.audns.net.au/au/status_available_spec.rb
@@ -23,22 +23,28 @@ describe Whois::Parsers::WhoisAudnsNetAu, "status_available.expected" do
 
   describe "#disclaimer" do
     it do
-      expect { subject.disclaimer }.to raise_error(Whois::AttributeNotSupported)
+      expect(subject.disclaimer).to eq("Afilias Australia Pty Ltd (Afilias), for itself and on behalf of .au Domain Administration Limited (auDA), makes the WHOIS registration data directory service (WHOIS Service) available solely for the purposes of:\n(a) querying the availability of a domain name licence;\n(b) identifying the holder of a domain name licence; and/or\n(c) contacting the holder of a domain name licence in relation to that domain name and its use.\nThe WHOIS Service must not be used for any other purpose (even if that purpose is lawful), including:\n(a) aggregating, collecting or compiling information from the WHOIS database, whether for personal or commercial purposes;\n(b) enabling the sending of unsolicited electronic communications; and / or\n(c) enabling high volume, automated, electronic processes that send queries or data to the systems of Afilias, any registrar, any domain name licence holder, or auDA.\nThe WHOIS Service is provided for information purposes only. By using the WHOIS Service, you agree to be bound by these terms and conditions. The WHOIS Service is operated in accordance with the auDA WHOIS Policy (available at https://www.auda.org.au/policies/index-of-published-policies/2014/2014-07/ ).")
     end
   end
   describe "#domain" do
     it do
-      expect(subject.domain).to eq(nil)
+      expect(subject.domain).to eq("GOOGLE.COM.AU")
     end
   end
   describe "#domain_id" do
     it do
-      expect { subject.domain_id }.to raise_error(Whois::AttributeNotSupported)
+      expect(subject.domain_id).to eq(nil)
     end
   end
   describe "#status" do
     it do
-      expect(subject.status).to eq([])
+      expect(subject.status).to eq([
+        "clientDeleteProhibited https://afilias.com.au/get-au/whois-status-codes#clientDeleteProhibited",
+        "clientUpdateProhibited https://afilias.com.au/get-au/whois-status-codes#clientUpdateProhibited",
+        "serverDeleteProhibited https://afilias.com.au/get-au/whois-status-codes#serverDeleteProhibited",
+        "serverRenewProhibited https://afilias.com.au/get-au/whois-status-codes#serverRenewProhibited",
+        "serverUpdateProhibited https://afilias.com.au/get-au/whois-status-codes#serverUpdateProhibited"
+      ])
     end
   end
   describe "#available?" do
@@ -56,11 +62,6 @@ describe Whois::Parsers::WhoisAudnsNetAu, "status_available.expected" do
       expect { subject.created_on }.to raise_error(Whois::AttributeNotSupported)
     end
   end
-  describe "#updated_on" do
-    it do
-      expect(subject.updated_on).to eq(nil)
-    end
-  end
   describe "#expires_on" do
     it do
       expect { subject.expires_on }.to raise_error(Whois::AttributeNotSupported)
@@ -68,13 +69,32 @@ describe Whois::Parsers::WhoisAudnsNetAu, "status_available.expected" do
   end
   describe "#registrar" do
     it do
-      expect(subject.registrar).to eq(nil)
+      expect(subject.registrar).to be_a(Whois::Parser::Registrar)
+      expect(subject.registrar.name).to eq("MarkMonitor Corporate Services Inc")
+      expect(subject.registrar.organization).to eq(nil)
+      expect(subject.registrar.url).to eq(nil)
     end
   end
   describe "#registrant_contacts" do
     it do
       expect(subject.registrant_contacts).to be_a(Array)
-      expect(subject.registrant_contacts).to eq([])
+      expect(subject.registrant_contacts.size).to eq(1)
+      expect(subject.registrant_contacts[0]).to be_a(Whois::Parser::Contact)
+      expect(subject.registrant_contacts[0].type).to eq(Whois::Parser::Contact::TYPE_REGISTRANT)
+      expect(subject.registrant_contacts[0].id).to eq("MMR-122026")
+      expect(subject.registrant_contacts[0].name).to eq("Domain Administrator")
+      expect(subject.registrant_contacts[0].organization).to eq("Google INC")
+      expect(subject.registrant_contacts[0].address).to eq(nil)
+      expect(subject.registrant_contacts[0].city).to eq(nil)
+      expect(subject.registrant_contacts[0].zip).to eq(nil)
+      expect(subject.registrant_contacts[0].state).to eq(nil)
+      expect(subject.registrant_contacts[0].country).to eq(nil)
+      expect(subject.registrant_contacts[0].country_code).to eq(nil)
+      expect(subject.registrant_contacts[0].phone).to eq(nil)
+      expect(subject.registrant_contacts[0].fax).to eq(nil)
+      expect(subject.registrant_contacts[0].email).to eq(nil)
+      expect(subject.registrant_contacts[0].created_on).to eq(nil)
+      expect(subject.registrant_contacts[0].updated_on).to eq(nil)
     end
   end
   describe "#admin_contacts" do
@@ -85,13 +105,37 @@ describe Whois::Parsers::WhoisAudnsNetAu, "status_available.expected" do
   describe "#technical_contacts" do
     it do
       expect(subject.technical_contacts).to be_a(Array)
-      expect(subject.technical_contacts).to eq([])
+      expect(subject.technical_contacts.size).to eq(1)
+      expect(subject.technical_contacts[0]).to be_a(Whois::Parser::Contact)
+      expect(subject.technical_contacts[0].type).to eq(Whois::Parser::Contact::TYPE_TECHNICAL)
+      expect(subject.technical_contacts[0].id).to eq("MMR-87489")
+      expect(subject.technical_contacts[0].name).to eq("DNS Admin")
+      expect(subject.technical_contacts[0].organization).to eq(nil)
+      expect(subject.technical_contacts[0].address).to eq(nil)
+      expect(subject.technical_contacts[0].city).to eq(nil)
+      expect(subject.technical_contacts[0].zip).to eq(nil)
+      expect(subject.technical_contacts[0].state).to eq(nil)
+      expect(subject.technical_contacts[0].country).to eq(nil)
+      expect(subject.technical_contacts[0].country_code).to eq(nil)
+      expect(subject.technical_contacts[0].phone).to eq(nil)
+      expect(subject.technical_contacts[0].fax).to eq(nil)
+      expect(subject.technical_contacts[0].email).to eq(nil)
+      expect(subject.technical_contacts[0].created_on).to eq(nil)
+      expect(subject.technical_contacts[0].updated_on).to eq(nil)
     end
   end
   describe "#nameservers" do
     it do
       expect(subject.nameservers).to be_a(Array)
-      expect(subject.nameservers).to eq([])
+      expect(subject.nameservers.size).to eq(4)
+      expect(subject.nameservers[0]).to be_a(Whois::Parser::Nameserver)
+      expect(subject.nameservers[0].name).to eq("NS1.GOOGLE.COM")
+      expect(subject.nameservers[1]).to be_a(Whois::Parser::Nameserver)
+      expect(subject.nameservers[1].name).to eq("NS2.GOOGLE.COM")
+      expect(subject.nameservers[2]).to be_a(Whois::Parser::Nameserver)
+      expect(subject.nameservers[2].name).to eq("NS3.GOOGLE.COM")
+      expect(subject.nameservers[3]).to be_a(Whois::Parser::Nameserver)
+      expect(subject.nameservers[3].name).to eq("NS4.GOOGLE.COM")
     end
   end
 end
